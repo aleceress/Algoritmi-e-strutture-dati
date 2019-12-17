d6c45423b90dbbb18bd8c290370373d49adca2c0
qwertium <58306137+qwertium@users.noreply.github.com>
2019-12-17 11:31:29 +0100

    Update proprietà.c

--- proprietà.c
+++ proprietà.c
@@ -5,12 +5,19 @@ Graph gen(float p, int n);
 int degree(Graph g,int v);
 int dfspath(Graph g, int v, int w, int *camm);
 int path(Graph g,int v, int w);
+void cccdfs(Graph g, int i, int *cc, int id); 
+int ccc(Graph G);
 
 int main (void) {
-    Graph g = gen(0.2,4);
+    Graph g = graph_new(5);
+    graph_edgeinsert(g,0,1);
+    graph_edgeinsert(g,1,3);
+    graph_edgeinsert(g,3,0);
+    graph_edgeinsert(g,2,4);
+
     graph_print(g);
     //printf("%d\n",degree(g,3));
-    printf("%d\n",path (g,1,3));
+    printf("%d\n", ccc(g));
 }
 
 Graph gen(float p, int n) {
@@ -67,3 +74,38 @@ int path(Graph g,int v, int w) {
     }
     return 0; 
 }
+
+void cccdfs(Graph g, int i, int *cc, int id) {   
+    int j;
+    cc[i] = id;
+    for (j=0;j< g->n; j++) {
+        if (g->A[i][j] &&  (cc[j] == -1)) {
+            cc[j] = id; 
+            cccdfs(g,j,cc,id); 
+        } 
+    }  
+}
+
+int ccc(Graph g) {
+    int i,j, id = 0; 
+    int max; 
+    int *cc = malloc(g -> n*sizeof(int));   
+    for (int i = 0; i< g -> n; i++) 
+        cc[i] = -1; 
+
+    for (i=0;i< g->n; i++) {
+        for (j=0;j< g->n; j++) {
+            if (g->A[i][j] && cc[i] == -1) {
+                cccdfs(g,j,cc,id++);
+            }
+        }
+    }
+
+    max = cc[0];
+    for (i=1; i< g-> n; i++) {
+        if (cc[i] > max)
+            max = cc[i];
+    }
+
+    return max+1; 
+} 
