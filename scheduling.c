f22ca6f0b429fdde9c0153c5910ec0bc403abaaa
qwertium <58306137+qwertium@users.noreply.github.com>
2019-12-23 18:18:43 +0100

    Update scheduling.c

--- scheduling.c
+++ scheduling.c
@@ -11,6 +11,9 @@ struct intervallo {
 void ordinaIntervalli(struct intervallo *intervalli, int n); 
 void scambia(int *a, int *b); 
 int *createP(struct intervallo *intervalli, int n);
+int *ComputeOpt(int *p, int n, struct intervallo *intervalli);
+void FindSolution(int j, int *opt, int *p, struct intervallo *intervalli); 
+
 
 int main (void) {
     int numero_intervalli;
@@ -29,10 +32,51 @@ int main (void) {
     }
 
     ordinaIntervalli(intervalli,numero_intervalli); 
+
+    for (j = 0; j< numero_intervalli;j++) {
+        printf("%d ",intervalli[j].v);
+    }
+
+    printf("\n"); 
+
     int *p = createP(intervalli, numero_intervalli);
 
     for (i=0; i<numero_intervalli;i++) 
         printf("%d ",p[i]); 
+
+    int *opt = ComputeOpt(p,numero_intervalli, intervalli);
+
+    printf("\n"); 
+
+    for (i=0; i<numero_intervalli;i++) 
+        printf("%d ",opt[i]);
+
+    printf("\n"); 
+
+    FindSolution(numero_intervalli - 1, opt, p, intervalli); 
+}
+
+void FindSolution(int j, int *opt, int *p, struct intervallo *intervalli) {
+    if (j!=0) {
+        if (opt[j] + intervalli[j].v >= opt[j-1]) {
+            printf("%d-%d %d\n", intervalli[j].i, intervalli[j].f, intervalli[j].v); 
+            FindSolution(p[j], opt, p, intervalli); 
+        } else {
+            FindSolution(j-1, opt, p, intervalli); 
+        }  
+    }
+}
+
+int *ComputeOpt(int *p, int n, struct intervallo *intervalli) {
+    int *opt = calloc(n,sizeof(int)); 
+    int i; 
+    for(i=1; i<n;i++) {
+        if (intervalli[i].v + opt[p[i]] > opt[i-1])
+            opt[i] = intervalli[i].v + opt[p[i]]; 
+            else
+                opt[i] = opt[i-1];
+    }
+    return opt; 
 }
 
 int *createP(struct intervallo *intervalli, int n) {
@@ -44,9 +88,7 @@ int *createP(struct intervallo *intervalli, int n) {
         for (i=0; i < j; i++) {
             if ((intervalli[j].f <= intervalli[i].i || intervalli[j].i >= intervalli[i].f) && intervalli[i].v > max) {
                 max = intervalli[i].v; 
-                printf("%d\n", max);
                 p[j] = i;  
-                printf("ciao\n"); 
             }
         }
     }
@@ -62,12 +104,18 @@ void scambia(int *a, int *b) {
 }
 
 void ordinaIntervalli(struct intervallo *intervalli, int n) {
-    int i, j; 
-    for(i = 0; i < n - 1; i++) 
-      for (j = n - 1; j > i; j--)
-                  if (intervalli[j].f < intervalli[j-1].f) {
-                          scambia(&intervalli[j].f, &intervalli[j-1].f); 
-                          scambia(&intervalli[j].i, &intervalli[j-1].i); 
-                          scambia(&intervalli[j].v, &intervalli[j-1].v); 
-                  }
+    int i, ti, tf, tv; 
+    int scambiato; 
+    do { 
+        scambiato = 0;
+        for (i=1; i< n; i++) {
+            if (intervalli[i].v < intervalli[i-1].v) {
+                printf("ciao\n"); 
+                scambia(&intervalli[i].i, &intervalli[i-1].i); 
+                scambia(&intervalli[i].f, &intervalli[i-1].f); 
+                scambia(&intervalli[i].v, &intervalli[i-1].v); 
+                scambiato = 1; 
+            }
+        }
+    } while (scambiato); 
 } 
